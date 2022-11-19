144edbac248fabd055afbdd38a0aa8772baa9bc2
Emanuel Jöbstl <emanuel.joebstl@gmail.com>
2022-11-19 21:22:23 +0100

    Initial commit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [email, setEmail] = useState('')
+  const [handle, setHandle] = useState('')
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  const submit = async () => {
+    // TODO: Error handling & param validation
+    const resp = await fetch("https://api-development.authory.com/twitter-import/signup", {
+      method: "post",
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        twitterUrl: handle
+        // TODO: Add recaptcha v3?
+      })
+    })
+
+    if(resp.status === 200) {
+      setIsSubmitted(true)
+    } else {
+      console.error(resp.status, resp.statusText, await resp.text())
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,45 +42,35 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Back up your Twitter!
         </h1>
 
+        <div style={{ marginTop: 30 }} className={styles.collumn}>
+          <form className={styles.collumn}>
+            <input type="email" className="form-control" id="email" placeholder='Your Email' value={email} onChange={e => setEmail(e.target.value)}></input>
+            <br></br>
+            <input type="text" className="form-control" id="twitter" placeholder='Your Twitter Handle' value={handle} onChange={e => setHandle(e.target.value)}></input>
+            <br></br>
+            <input type="button" className="form-control btn btn-primary" value="Submit" onClick={submit}></input>
+          </form>
+        </div>
+
+        { isSubmitted && <p><b>Thank you, you will receive an email shortly.</b></p>}
+        
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          This will fully backup your twitter, 
+          including media, into a searchable archive that <a href="#">looks like this</a>.
+          <br />We will send you a link to your profile via email as soon as it's ready.
+          <br /><br />
+          This service is free for a year, you will be able to download all data.
+          <br /><br />
+
+          After a year, we will contact you and
+          ask you to turn your backed up twitter feed into a full <a href="#">Authory</a> account.
+          <br /><br />
+          We built this for the same reason we built <a href="#">Authory</a> - we belive in free speach and fully owning your own data.
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
